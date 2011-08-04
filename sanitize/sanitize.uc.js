fb85bcfac53b0225640d83179440a225fc15b6e2
Arne Dieckmann <sobotnik@gmx.net>
2011-08-04 21:21:10 +0200

    Stoerende Tabmix-Einstellung entfernt, Eintragung in JavaScript-Konsole.

--- sanitize/sanitize.uc.js
+++ sanitize/sanitize.uc.js
@@ -1,5 +1,6 @@
 WindowHook.register("chrome://browser/content/sanitize.xul", function(aWindow) {
   with(aWindow) {
+    userChrome.log("sanitize.uc.js verhindert bewusst bestimmte Änderungen in dem Fenster zum Löschen der Chronik (aufgerufen mit Strg+Umschalt+Entf).");
     document.getElementsByAttribute("preference", "privacy.cpd.history")[0].disabled = true;
     document.getElementsByAttribute("preference", "privacy.cpd.formdata")[0].disabled = true;
     document.getElementsByAttribute("preference", "privacy.cpd.cookies")[0].disabled = true;    
@@ -11,9 +12,12 @@ WindowHook.register("chrome://browser/content/sanitize.xul", function(aWindow) {
 
 WindowHook.register("chrome://browser/content/preferences/sanitize.xul", function(aWindow) {
   with(aWindow) {
-    document.getElementsByAttribute("preference", "privacy.item.extensions-tabmix")[0].disabled = true;
+    userChrome.log("sanitize.uc.js verhindert bewusst bestimmte Änderungen in dem Fenster Extras/Einstellungen/Datenschutz/Angaben zum Löschen der Chronik.");
+    document.getElementsByAttribute("preference", "privacy.clearOnShutdown.cookies")[0].checked = false;
+    document.getElementsByAttribute("preference", "privacy.clearOnShutdown.formdata")[0].checked = false;
+    document.getElementsByAttribute("preference", "privacy.clearOnShutdown.history")[0].checked = false;
+
     document.getElementsByAttribute("preference", "privacy.clearOnShutdown.history")[0].disabled = true;
-  document.getElementById("privacy.clearOnShutdown.history")[0].disabled = true;
     document.getElementsByAttribute("preference", "privacy.clearOnShutdown.formdata")[0].disabled = true;
     document.getElementsByAttribute("preference", "privacy.clearOnShutdown.cookies")[0].disabled = true;
     document.getElementsByAttribute("preference", "privacy.clearOnShutdown.downloads")[0].disabled = true;
@@ -21,6 +25,6 @@ WindowHook.register("chrome://browser/content/preferences/sanitize.xul", functio
     document.getElementsByAttribute("preference", "privacy.clearOnShutdown.sessions")[0].disabled = true;
     document.getElementsByAttribute("preference", "privacy.clearOnShutdown.siteSettings")[0].disabled = true;
     document.getElementsByAttribute("preference", "privacy.clearOnShutdown.offlineApps")[0].disabled = true;
+    document.getElementsByAttribute("preference", "privacy.clearOnShutdown.cache")[0].disabled = true;
   }
 });
-
