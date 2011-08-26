3ac5f966193a9b35866b128d03051d8c9d535b9d
Arne Dieckmann <sobotnik@gmx.net>
2011-08-26 16:08:32 +0200

    Skript - Erste Version

---
+++ moveorangebutton/moveOrangeButton.uc.js
@@ -0,0 +1,5 @@
+(function() {
+    var appButton = document.getElementById('appmenu-button-container');
+    var navBar = document.getElementById('nav-bar');
+    navBar.insertBefore(appButton, navBar.firstChild);
+})();
