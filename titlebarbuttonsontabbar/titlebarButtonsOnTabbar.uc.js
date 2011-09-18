f9f559b1e183bbdfc1219385b3461eb7c97749b3
Arne Dieckmann <sobotnik@gmx.net>
2011-09-18 15:53:07 +0200

    Skript - Erste Version

---
+++ titlebarbuttonsontabbar/titlebarButtonsOnTabbar.uc.js
@@ -0,0 +1,5 @@
+(function() {
+    var titleButtons = document.getElementById("titlebar-buttonbox");
+    var tabsBar = document.getElementById("TabsToolbar")
+    tabsBar.appendChild(titleButtons);
+})();
