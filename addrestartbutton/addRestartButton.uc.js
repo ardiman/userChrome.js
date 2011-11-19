1c6269ce79e326c6d290c76024388e209d3bc766
Arne Dieckmann <sobotnik@gmx.net>
2011-11-19 14:56:32 +0100

    Skript - Zweite Version

--- addrestartbutton/addRestartButton.uc.js
+++ addrestartbutton/addRestartButton.uc.js
@@ -16,7 +16,7 @@
 var ToolRstart = {
   //SAVE_SESSION_RESTART_VERSION: "0.0.2",
   init: function() {
-    if (document.getElementById("Restart_Firefox")) return;
+    if (document.getElementById("Restart_Firefox_withDelDevCache")) return;
     var optionsitem, menuitem, menupopup;
     var UI = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].
       createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
@@ -32,19 +32,19 @@ try{
       if (button){
         //button.setAttribute("disabled",false); //Fix With Rewindfowerd.xpi
         menuitem = document.createElement("menuitem");
-        menuitem.setAttribute("id", "Restart_Firefox0");
+        menuitem.setAttribute("id", "Restart_Firefox0__withDelDevCache");
         menuitem.setAttribute("class", "menuitem-iconic");
         menuitem.setAttribute("label", label);
         menuitem.setAttribute("image", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA0AAAANCAYAAABy6%2BR8AAAABGdBTUEAALGPC%2FxhBQAAAkVJREFUeJxVks1Lk3EAxz%2B%2FZ79ne575tk2ntjlsCnaY1aGC7hYWVPRyiA7RX1CEEdRp8xAFRnQI6tClSx0sCBEi8FDRC4ZpSaIpvsSWm07ndC8%2B255nT4cw6nv6Hj6f20fwz26%2FS3eXy5WB6JIRomSCEFwL13G8Uxs80uXp3%2BHEzrn8ajm2lrOjXodNq0eljMC0bMySxdt0hX1u5eGtk4GnrTWO92LJtrUHr1dujK2Uo6c6NHRd4hA2DiBv2hiWTblcZSJpcrTVkT1%2FoPGMHBlNhzdzVvREWEPTHHR5VPbu0tFVhfV8hYnUNlIIGnWTgW85T3PN2iOZL1bvGaaFpmm0uCX7m1QwDBSh0u51EmhQsW14NpnlsN%2FFuY%2BZihycy4Z7QnVkKzaudJ7HMym%2Bb5o0WyZXettoD%2FkAWEgZrFZs%2FIaFzG0VzaThxue26d1dS%2BfBJsyq4PnXddJlBT1jYCsKqRKMFIBiCWnlCnIlV8%2B2W0d1VHErZTbyJXQqXP1Swl%2FN0O2RTKo6%2FnpIFw0pTwe0xZeJ7B5PXQ3xdAGRqeDz6fR0KBwKCkBh7JfFjNQhvgWbhUWZTBb6WnIcK2byPFktkljY5GLEQdDvRFEUfmbhkzeE2iDZmE6SHl3uE%2FdfzLmG3szfnMUVTQSbweWEqkmbLgh7XASCXpBOpj8vMvlhNoa%2FeudvEZFLQzHTqUd%2FtPqgpQF%2FUy2RWgm5bZLTKQrj8Vhi%2BGz%2Ffxn9EYcjdW777vy22oYqAIHLKMUT4%2FnrTF2Y2uF%2BA5XB81eiCxQGAAAAAElFTkSuQmCC");
-        menuitem.setAttribute("oncommand", "ToolRstart.SaveRestart(event);");
+        menuitem.setAttribute("onclick", "ToolRstart.SaveRestart(event);");
         optionsitem = document.getElementById("btn-sm-settings");
         optionsitem.parentNode.insertBefore(menuitem, optionsitem);
       }
 
       menuitem = document.createElement("menuitem");
-      menuitem.setAttribute("id", "Restart_Firefox1");
+      menuitem.setAttribute("id", "Restart_Firefox1__withDelDevCache");
       menuitem.setAttribute("label", label);
-      menuitem.setAttribute("oncommand", "ToolRstart.SaveRestart(event);");
+      menuitem.setAttribute("onclick", "ToolRstart.SaveRestart(event);");
       optionsitem = document.getElementById("menu_FileQuitItem");
       optionsitem.parentNode.insertBefore(menuitem, optionsitem);
     }
@@ -54,7 +54,7 @@ try{
     menuitem = document.createElement("menuitem");
     menuitem.setAttribute("label", label);
     menuitem.setAttribute("accesskey", "R");
-    menuitem.setAttribute("oncommand", "ToolRstart.restartApp();");
+    menuitem.setAttribute("onclick", "ToolRstart.restartApp(event);");
     optionsitem = document.getElementById("menu_FileQuitItem");
     optionsitem.parentNode.insertBefore(menuitem, optionsitem);
     menuitem.setAttribute("id", "Restart_Firefox");
@@ -66,16 +66,19 @@ try{
     dump("Initialized addRestartButtons");
   },
 
-  SaveRestart: function(event) {
-    event.stopPropagation();
+  SaveRestart: function(e) {
+    e.stopPropagation();
     SessionManager.sessionUtil('save', 'allwindows');
-    ToolRstart.restartApp();
+    ToolRstart.restartApp(e);
   },
 
-  //sessionsaver_.2-0.2.1.031-fx+mz.xpiから
-  restartApp: function() {
+  //sessionsaver_.2-0.2.1.031-fx+mz.xpi??
+  restartApp: function(e) {
+    if (e.button !=0 ) {
+      let xRE = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime);
+      xRE.invalidateCachesOnRestart();
+    }
     const nsIAppStartup = Components.interfaces.nsIAppStartup;
-
     // Notify all windows that an application quit has been requested.
     var os = Components.classes["@mozilla.org/observer-service;1"]
                        .getService(Components.interfaces.nsIObserverService);
