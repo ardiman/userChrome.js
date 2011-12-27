c405937d95a42d9dbec3b09a968207400234596a
Arne Dieckmann <sobotnik@gmx.net>
2011-12-27 17:24:05 +0100

    Skript - angepasste Version für Firefox 9

---
+++ openprofiledirectory/OpenProfileDirectory_Fx9.uc.js
@@ -0,0 +1,22 @@
+// ==Userscript==
+// @name Open Profile Directory
+// @include *
+// ==/Userscript==
+
+(function(win){
+    function openProfileDirectory() {
+        Components.classes["@mozilla.org/file/directory_service;1"]
+              .getService(Components.interfaces.nsIProperties)
+              .get("ProfD", Components.interfaces.nsILocalFile)
+              .launch();
+    }
+    if(typeof win.openProfileDirectory == 'undefined') {
+        win.openProfileDirectory = openProfileDirectory;
+        win.addEventListener('keydown', function(e) {
+            if (e.altKey == true && e.keyCode == 80) {
+                e.preventDefault();
+                openProfileDirectory();
+            }
+        }, false);
+    }
+})(window);
