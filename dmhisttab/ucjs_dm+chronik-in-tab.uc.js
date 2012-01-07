2bf264bfc5bd11b3b89ac8ffea227c7a14d4748c
Arne Dieckmann <sobotnik@gmx.net>
2012-01-07 16:46:34 +0100

    Skript - Angepasste Version fuer Firefox 9

--- dmhisttab/ucjs_dm+chronik-in-tab.uc.js
+++ dmhisttab/ucjs_dm+chronik-in-tab.uc.js
@@ -1,82 +1,45 @@
+<!--
+// ==UserScript==
+// @name           ucjs_dm+chronik-in-tab.uc.js
+// @compatibility  Firefox 9.*
+// @version        1.0.20120107
+// ==/UserScript==
+-->
 
 /*
- *********************************************************************************
- ***** 21. Downloadmanager und Chronik im Tab oeffnen *******************
- *********************************************************************************
- *
- *** 21.01 Window Hook (ist fuer "DM in Hintergrundtab" noetig etc.) ***
- */
-
-
-var WindowHook = {
-	observe: function(aSubject, aTopic, aData)
-	{
-		if (!aSubject._WindowHook)
-		{
-			aSubject._WindowHook = this;
-			aSubject.addEventListener("load", this.onLoad_window, false);
-		}
-	},
-
-	onLoad_window: function()
-	{
-		this.removeEventListener("load", this._WindowHook.onLoad_window, false);
-		var funcs = this._WindowHook.mFuncs[this.document.location.href] || null;
-		if (funcs)
-		{
-			funcs.forEach(function(aFunc) { aFunc(this); }, this);
-		}
-		delete this._WindowHook;
-	},
-
-	register: function(aURL, aFunc)
-	{
-		if (!this.mFuncs)
-		{
-			this.mFuncs = {};
-			Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService).addObserver(this, "domwindowopened", false);
-		}
-		if (!this.mFuncs[aURL])
-		{
-			this.mFuncs[aURL] = [];
-		}
-		this.mFuncs[aURL].push(aFunc);
-	}
-};
-
-
-///// 21.02. diverse-Manager in Tab oeffnen
- 
- 
-// Bookmark Manager open in Tab //GEHT NICHT MEHR MIT FX3!!
-//document.getElementsById("Browser:ShowAllBookmarks").setAttribute("oncommand", '(getBrowser().selectedTab = //getBrowser().addTab("chrome://browser/content/places/places.xul"))');
-
-
-// Download Manager open in Tab // GEHT mit FX3!
-document.getElementById("Tools:Downloads").setAttribute("oncommand", '(getBrowser().selectedTab = getBrowser().addTab("chrome://mozapps/content/downloads/downloads.xul")).label = "Download Manager";');
-
-// History Manager open in Tab // GEHT mit FX3!
-document.getElementById("viewHistorySidebar").setAttribute("oncommand", '(getBrowser().selectedTab = getBrowser().addTab("chrome://browser/content/history/history-panel.xul")).label = "History Manager";');
-
-
-///// 21.03. Download-Manager in Hintergrundtab oeffnen
-
-WindowHook.register("chrome://mozapps/content/downloads/downloads.xul", function(aWindow) {
-   var browser = Components.classes["@mozilla.org/appshell/window-mediator;1"]
-      .getService(Components.interfaces.nsIWindowMediator)
-      .getMostRecentWindow("navigator:browser").getBrowser();
-   aWindow.addEventListener("unload", function() {
-      browser.addTab("chrome://mozapps/content/downloads/downloads.xul");
-   }, false);
-   aWindow.close();
-});
-
-
-
-
-/* im Vordergrund:
-...
-browser.selectedTab = browser.addTab("chrome://mozapps/content/downloads/downloads.xul"); 
+  Konfiguration des Tabverhaltens fuer Lesezeichen-, Download-, Historymanager
+    tabMode 0=nicht veraendern, 1=Tab im Vordergrund, 2=Tab im Hintergrund, 3= als Fenster
 */
+var configArray = [
+  {tabMode: 3,
+   searchedId: "bookmarksShowAll",
+   label: "Lesezeichen-Manager",
+   chromeUrl: "chrome://browser/content/places/places.xul"},
+  {tabMode: 1,
+   searchedId: "menu_openDownloads",
+   label: "Download-Manager",
+   chromeUrl: "chrome://mozapps/content/downloads/downloads.xul"},
+  {tabMode: 2,
+   searchedId: "menu_showAllHistory",
+   label: "History-Manager",
+   chromeUrl: "chrome://browser/content/history/history-panel.xul"},
+];
+// Ende der Konfiguration
 
 
+// Das configArray durchlaufen und je nach gewaehltem tabMode/Manager agieren
+for (var i = 0; i < this.configArray.length; i++) {
+  wId=document.getElementById(configArray[i]["searchedId"]);
+  wId.setAttribute("command", null);
+  switch (configArray[i]["tabMode"]) {
+    case 1:
+      wId.setAttribute("oncommand", "(getBrowser().selectedTab = getBrowser().addTab('"+configArray[i]['chromeUrl']+"'));");
+      break;
+    case 2:
+      wId.setAttribute("oncommand", "(getBrowser().selectedTab = getBrowser().addTab('"+configArray[i]['chromeUrl']+"').label = '"+configArray[i]['label']+"');");
+      break;
+    case 3:
+      wId.setAttribute("oncommand", "(getBrowser().selectedTab = window.open('"+configArray[i]['chromeUrl']+"'))");
+      break;
+  }
+}
