71b44d56526863ff13d132caab5995a766fbe32f
Arne Dieckmann <sobotnik@gmx.net>
2012-01-12 22:20:49 +0100

    Skript - Zweite Version

--- errorconsoleintab/errorconsoleintab.uc.js
+++ errorconsoleintab/errorconsoleintab.uc.js
@@ -1,6 +1,20 @@
-// Console in Tab.....................................................
-document.getElementById("javascriptConsole").setAttribute("oncommand", 'getBrowser (). selectedTab = getBrowser (). addTab ("chrome://global/content/console.xul")');
-document.getElementById("appmenu_errorConsole").setAttribute("oncommand", 'getBrowser (). selectedTab = getBrowser (). addTab ("chrome://global/content/console.xul")');
+<!--
+// ==UserScript==
+// @name           errorconsoleintab.uc.js
+// @compatibility  Firefox 8.*, 9.*
+// @include        main
+// @version        1.0.20120112
+// ==/UserScript==
+-->
 
-// fuer Benutzer der Erweiterung "Web Developer"
-// document.getElementById("webdeveloper-error-console-toolbar").setAttribute("oncommand", 'getBrowser (). selectedTab = getBrowser (). addTab ("chrome://global/content/console.xul")');
+ucjs_errcontab = {
+  init: function() {
+    const err_con_ids = ['javascriptConsole','appmenu_errorConsole','webdeveloper-error-console-toolbar'];
+    const sCmd = 'getBrowser (). selectedTab = getBrowser (). addTab ("chrome://global/content/console.xul")';
+    for (var i = 0; i < err_con_ids.length; i++) {
+      var ele=document.getElementById(err_con_ids[i]);
+      if (ele!=null) ele.setAttribute("oncommand",sCmd);
+    }
+  }
+};
+ucjs_errcontab.init();
