3f0304e07a7c2dbd2ec1a1385b714767b1731a8b
Arne Dieckmann <sobotnik@gmx.net>
2012-07-28 22:46:21 +0200

    Skript - technisches Update

--- showcompleteurl/ShowCompleteURL.uc.js
+++ showcompleteurl/ShowCompleteURL.uc.js
@@ -2,5 +2,5 @@ location=="chrome://mozapps/content/downloads/unknownContentType.xul"&&(function
 	s.value = dialog.mLauncher.source.spec;
 	s.setAttribute("crop", "center");
 	s.setAttribute("tooltiptext", dialog.mLauncher.source.spec);
-	s.setAttribute("ondblclik", Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString(dialog.mLauncher.source.spec))
+	s.addEventListener("dblclick", function(){ Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString(dialog.mLauncher.source.spec)} , false)
 })(document.querySelector("#source"))
