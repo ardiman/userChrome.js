0df694e7f6471cbd05c7a021c559a95e895676cc
Arne Dieckmann <sobotnik@gmx.net>
2012-10-18 18:48:04 +0200

    Skript - Anpassung wg. Webdeveloper-Erweiterung

--- errorconsoleintab/errorconsoleintab.uc.js
+++ errorconsoleintab/errorconsoleintab.uc.js
@@ -1,17 +1,21 @@
 // ==UserScript==
 // @name           errorconsoleintab.uc.js
-// @compatibility  Firefox 8.*, 9.*
+// @compatibility  Firefox 8.*, 9.*, 16.*
 // @include        main
-// @version        1.0.20120112
+// @version        1.0.20121018
 // ==/UserScript==
 
 ucjs_errcontab = {
   init: function() {
-    const err_con_ids = ['javascriptConsole','appmenu_errorConsole','webdeveloper-error-console-toolbar'];
+    const err_con_ids = ['javascriptConsole','appmenu_errorConsole','web-developer-error-console-toolbar'];
     const sCmd = 'getBrowser (). selectedTab = getBrowser (). addTab ("chrome://global/content/console.xul")';
     for (var i = 0; i < err_con_ids.length; i++) {
       var ele=document.getElementById(err_con_ids[i]);
-      if (ele!=null) ele.setAttribute("oncommand",sCmd);
+      if (ele!=null) {
+        ele.setAttribute("command",null);
+        ele.setAttribute("label","Fehlerkonsole");
+        ele.setAttribute("oncommand",sCmd);
+      }
     }
   }
 };
