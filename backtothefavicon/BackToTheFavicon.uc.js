a2ee1cc33b41649d56eee08f4e949331a8d1413d
Arne Dieckmann <sobotnik@gmx.net>
2012-11-04 12:25:41 +0100

    Skript - Zweite Version

--- backtothefavicon/BackToTheFavicon.uc.js
+++ backtothefavicon/BackToTheFavicon.uc.js
@@ -1,4 +1,5 @@
 (function() {
+    if (location != "chrome://browser/content/browser.xul") return;
     gBrowser.tabContainer.addEventListener("TabAttrModified", function() {
         if (gBrowser.mCurrentTab.image === gProxyFavIcon.src) return;
         (!!gBrowser.mCurrentTab.image) ? gProxyFavIcon.src = gBrowser.mCurrentTab.image : gProxyFavIcon.removeAttribute("src");
