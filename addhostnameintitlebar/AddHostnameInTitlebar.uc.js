6cf1f27c50b8dc78e9c905c9e533eadc2d5a8f58
Arne Dieckmann <sobotnik@gmx.net>
2013-01-24 20:06:34 +0100

    Skript - Erste Version

---
+++ addhostnameintitlebar/AddHostnameInTitlebar.uc.js
@@ -0,0 +1,8 @@
+﻿
+/*AddHostnameInTitlebar.uc.js*/
+
+eval('gBrowser.getWindowTitleForBrowser = ' + gBrowser.getWindowTitleForBrowser.toString().replace('aBrowser.contentTitle;', 'this.contentDocument.domain + " | " + aBrowser.contentTitle;'));
+
+
+
+
