1824819cc90fb719474ae77c41c7166fc77c4cd5
Arne Dieckmann <sobotnik@gmx.net>
2013-01-25 17:14:51 +0100

    Skript - technisches Update

--- addhostnameintitlebar/AddHostnameInTitlebar.uc.js
+++ addhostnameintitlebar/AddHostnameInTitlebar.uc.js
@@ -1,8 +1,6 @@
 ﻿
 /*AddHostnameInTitlebar.uc.js*/
 
-eval('gBrowser.getWindowTitleForBrowser = ' + gBrowser.getWindowTitleForBrowser.toString().replace('aBrowser.contentTitle;', 'this.contentDocument.domain + " | " + aBrowser.contentTitle;'));
-
-
+eval('gBrowser.getWindowTitleForBrowser = ' + gBrowser.getWindowTitleForBrowser.toString().replace('aBrowser.contentTitle;', 'this.contentDocument.location.hostname + " | " + aBrowser.contentTitle;'));
 
 
