8a6d77cf51aeeb04b42d21dbb9b3a3cb2e9b49fc
Arne Dieckmann <sobotnik@gmx.net>
2013-02-12 20:23:28 +0100

    Skript - Zweite Version

--- statusbarclock/statusbarclock.uc.js
+++ statusbarclock/statusbarclock.uc.js
@@ -1,40 +1,28 @@
 function Clock() {
-    var days = ["So","Mo", "Di", "Mi", "Do", "Fr", "Sa"];
-    var months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
     var D = new Date();
+    var days = ["So","Mo", "Di", "Mi", "Do", "Fr", "Sa"];
     var day = days[D.getDay()];
-    var month = months[D.getMonth()];
-    var year = D.getFullYear();
     var hours = D.getHours();
-    var min = D.getMinutes();
-    var sec = D.getSeconds(); 
-    var Time = ((hours < 10) ? "0" : "") + hours;
-    Time += ((min < 10) ? ":0" : ":") + min;
-    Time += " Uhr";
-    var date = day + ", " + ((D.getDate()<10) ? "0" : "") + D.getDate() + "."+ month + "." + year;
-    var timestr = date + " - " + Time;
-    var status = document.getElementById("statusbar-clock-display");
-    status.setAttribute("value", timestr);
-    var clockstyle = "padding-top: 2px;"
+    var dateString = day + ", " + ("0"+D.getDate()).substr(-2) + "." + ("0"+(D.getMonth()+1)).substr(-2) + "." + D.getFullYear();
+    var timeString = ("0"+hours).substr(-2) + ":" + ("0"+D.getMinutes()).substr(-2) + " Uhr";
+    var statusC = document.getElementById("statusbar-clock-display");
+    statusC.setAttribute("label", dateString + " - " + timeString);
     if (hours < 6 || hours > 22) {
-      clockstyle += "color: #ff0000;";
+      statusC.setAttribute("style", "color: #ff0000;");
       }
      else {
-       clockstyle += "color: #000000;";
+      statusC.setAttribute("style", "color: #000000;");
      }
-     status.setAttribute("style", clockstyle);
      // Timeout variabel - damit Umschalten zur vollen Minute:
-     setTimeout("Clock()", (60- sec)*1000);
+     setTimeout("Clock()", (60- D.getSeconds())*1000);
   }
-
-// letztes Kindelement der Statusbar/Add-on Bar finden
-// In Firefox 5 und mit installierter Erweiterung Status-4-Evar diese Zeile aktivieren:
-var ClockStatus = document.getElementById("status-bar").lastChild;
-//Ohne o.g. Erweiterung diese Zeile aktivieren:
-// var ClockStatus = document.getElementById("addon-bar").lastChild;
-var ClockLabel = document.createElement("label");
-ClockLabel.setAttribute("id", "statusbar-clock-display");
-ClockLabel.setAttribute("class", "statusbarpanel-text");
-// *vor* dem letzten Kindelement (s.o.) einfuegen - damit ist Uhr immer ganz rechts:
-ClockStatus.parentNode.insertBefore(ClockLabel, ClockStatus.previousSibling);
-Clock();
+if (location == "chrome://browser/content/browser.xul") {
+  // letztes Kindelement der Statusbar finden
+  var ClockStatus = document.getElementById("status-bar").lastChild;
+  // var ClockStatus = document.getElementById("addon-bar").lastChild;
+  var ClockPanel = document.createElement("statusbarpanel");
+  ClockPanel.setAttribute("id", "statusbar-clock-display");
+  // *vor* dem letzten Kindelement (s.o.) einfuegen - damit ist Uhr immer ganz rechts:
+  ClockStatus.parentNode.insertBefore(ClockPanel,ClockStatus.previousSibling);
+  Clock();
+}
