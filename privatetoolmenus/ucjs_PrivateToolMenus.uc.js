9555e17efcd42cf9b3548e119e3bac73f7698e05
Arne Dieckmann <sobotnik@gmx.net>
2013-05-14 21:27:22 +0200

    Skript - Update wg. Firefox 21

--- privatetoolmenus/ucjs_PrivateToolMenus.uc.js
+++ privatetoolmenus/ucjs_PrivateToolMenus.uc.js
@@ -3,25 +3,25 @@
 
 
 [
-	{
-		"label"    : "Show Cookies",
-		"accesskey": "K",
-		"oncommand": "window.open('chrome://browser/content/preferences/cookies.xul', 'Browser:Cookies', 'chrome,resizable=yes');"
-	},
-	{
-		"label"    : "Show Passwords",
-		"accesskey": "P",
-		"oncommand": "window.open('chrome://passwordmgr/content/passwordManager.xul', 'Toolkit:PasswordManager', 'chrome,resizable=yes');"
-	},
-	{
-		"label"    : "View Certificates",
-		"accesskey": "E",
-		"oncommand": "window.open('chrome://pippki/content/certManager.xul', 'mozilla:certmanager', 'chrome,resizable=yes,all,width=600,height=400');"
-	},
+{
+"label" : "Cookies öffnen",
+"accesskey": "C",
+"oncommand": "window.open('chrome://browser/content/preferences/cookies.xul', 'Browser:Cookies', 'chrome,resizable=yes');"
+},
+{
+"label" : "Passwörter öffnen",
+"accesskey": "P",
+"oncommand": "window.open('chrome://passwordmgr/content/passwordManager.xul', 'Toolkit:PasswordManager', 'chrome,resizable=yes');"
+},
+{
+"label" : "Zertifikate öffnen",
+"accesskey": "Z",
+"oncommand": "window.open('chrome://pippki/content/certManager.xul', 'mozilla:certmanager', 'chrome,resizable=yes,all,width=600,height=400');"
+},
 ]
 .forEach(function(attrs) {
-	var menuitem = document.createElement("menuitem");
-	for (var key in attrs)
-		menuitem.setAttribute(key, attrs[key]);
-	document.getElementById("menu_ToolsPopup").insertBefore(menuitem, document.getElementById("sanitizeItem"));
+var menuitem = document.createElement("menuitem");
+for (var key in attrs)
+menuitem.setAttribute(key, attrs[key]);
+document.getElementById("menu_ToolsPopup").insertBefore(menuitem, document.getElementById("menu_preferences"));
 });
