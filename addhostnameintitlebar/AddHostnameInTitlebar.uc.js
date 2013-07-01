45797db51889a1795262164d53b5375e87bd340f
ardiman <sobotnik@gmx.net>
2013-07-01 21:05:59 +0200

    Skript - technisches Update

--- addhostnameintitlebar/AddHostnameInTitlebar.uc.js
+++ addhostnameintitlebar/AddHostnameInTitlebar.uc.js
@@ -1,6 +1,15 @@
-﻿
-/*AddHostnameInTitlebar.uc.js*/
+// ==UserScript==
+// @name           AddHostnameInTitlebar.uc.js
+// @description    閲覧ページのホスト名をタイトルバーの先頭に表示
+// @namespace    http://u6.getuploader.com/script/  
+// @author         Anonymous
+// @include        main
+// @compatibility  Firefox 22
+// @charset        UTF-8
+// @version        3
+// @note           http://mimizun.com/log/2ch/software/1280677666/62
+// @note           http://mimizun.com/log/2ch/software/1362227410/785
+// @note           http://mimizun.com/log/2ch/software/1362227410/788
+// ==/UserScript==
 
-eval('gBrowser.getWindowTitleForBrowser = ' + gBrowser.getWindowTitleForBrowser.toString().replace('aBrowser.contentTitle;', 'this.contentDocument.location.hostname + " | " + aBrowser.contentTitle;'));
-
-
+eval('gBrowser.getWindowTitleForBrowser = ' + gBrowser.getWindowTitleForBrowser.toString().replace('aBrowser.contentTitle.replace("\\0", "", "g");', 'this.contentDocument.location.hostname + " | " + aBrowser.contentTitle;'));
