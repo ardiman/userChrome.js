4e13c753cf5d6699a253bc924913e7a7e51ae6e5
ardiman <sobotnik@gmx.net>
2013-09-29 17:56:13 +0200

    Skript - kein leerer Tab beim Wiederherstellen mehr

--- undoclosetabbymiddleclick/UndoCloseTabByMiddleClick.uc.js
+++ undoclosetabbymiddleclick/UndoCloseTabByMiddleClick.uc.js
@@ -1,3 +1,3 @@
-location == "chrome://browser/content/browser.xul" && (document.getElementById("tabbrowser-tabs") || gBrowser.mTabBox).addEventListener('click', function (e) {
-	e.button == 1 && e.originalTarget.localName == "box" && ((Application.version[0] > 3 && gBrowser.removeTab(gBrowser.mCurrentTab)) & undoCloseTab());
+location == "chrome://browser/content/browser.xul" && (document.getElementById("tabbrowser-tabs") || gBrowser.mTabBox).addEventListener('click', function (event) {
+   event.button == 1 && event.originalTarget.localName === "box" && ((parseInt(Application.version) > 3 && gBrowser.removeTab(gBrowser.mCurrentTab)) & undoCloseTab());
 }, false)
