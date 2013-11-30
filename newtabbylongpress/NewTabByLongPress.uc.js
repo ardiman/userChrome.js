ef2aa859d7e1b0900b37cd3e9e4f908e8f082d7f
ardiman <sobotnik@gmx.net>
2013-11-30 20:46:23 +0100

    Skript - technisches Update

--- newtabbylongpress/NewTabByLongPress.uc.js
+++ newtabbylongpress/NewTabByLongPress.uc.js
@@ -2,7 +2,7 @@
 // @name                newTabByLongPress.uc.js
 // @description         リンクやブックマークを左ボタン長押しで新しいタブに開く
 // @include             main
-// @version             0.6 リンクを現在のタブの右隣に開くか設定できるように
+// @version             0.7 エラーが出ることがあったので例外処理を追加
 // ==/UserScript==
 (function func() {
 
@@ -10,29 +10,30 @@
     const background = true;    // "false" für das Öffnen im Vordergrund
     const nextToCurrent = true;  // Direkt neben dem aktuellen Tab öffnen
 
-    var opened;
+    var opened = false;
 
     function isLink(node) {
         while (node) {
-            if ((node instanceof HTMLAnchorElement || node instanceof HTMLAreaElement) && node.hasAttribute('href')) {
-                if (/^\s*javascript:/.test(node.getAttribute('href')))
-                    return false;
+            if ((node instanceof HTMLAnchorElement || node instanceof HTMLAreaElement) && node.href) {
+//                if (/^\s*javascript:/.test(node.getAttribute('href')))
+//                    return false;
                 return node.href;
             }
             node = node.parentNode;
         }
-        return false;
+        return null;
     }
 
     function isPlaces(e, node) {
-        var ln = node.localName;
+        if (!node || !node.localName)
+            return null;
+        var ln = node.localName.toLowerCase();
         if (
             !('type' in node && node.type == 'menu') && (node._placesNode && PlacesUtils.nodeIsURI(node._placesNode))
             || ln == 'treechildren' && (node.parentNode.id == 'bookmarks-view' || node.parentNode.id == 'historyTree')
            )
-        var uri = ln == 'treechildren' ? getTreeInfo(node, e, 'uri') : node._placesNode.uri;
-            return uri;
-        return null;
+        var uri = (ln == 'treechildren') ? getTreeInfo(node, e, 'uri') : node._placesNode.uri;
+        return uri;
     }
 
     function getTreeInfo(treechildren, e, prop) {
@@ -48,35 +49,33 @@
         return node[prop];
     }
 
+    function cancel(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
     function handleLongPress(e) {
         setTimeout(function() {
             if (func.removeListener)
                 func.removeListener();
-        }, 0)
+        }, 0);
 
         var node = e.target || e.originalTarget;
+        if (!node) return;
         var url = isLink(node) || isPlaces(e, node);
         if (e.button != 0 || !url) return;
 
         if ((e.type == 'mousedown' && !opened) && (!e.altKey && !e.ctrlKey && !e.shiftKey)) {
             func.timer = setTimeout(function() {
-                function cancel(e) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                };
-
                 addEventListener('click', cancel, true);
-
                 func.removeListener = function() {
-                    removeEventListener('click', cancel, true)
+                    removeEventListener('click', cancel, true);
                 };
-
                 gBrowser.loadOneTab(url, {
                     relatedToCurrent: nextToCurrent,
                     inBackground: background,
                     referrerURI: makeURI(content.location.href)
                 });
-
                 opened = true;
             }, wait);
         } else {
@@ -88,8 +87,6 @@
         }
     }
 
-        addEventListener('mousedown', handleLongPress, true, gBrowser);
-        addEventListener('mouseup', handleLongPress, true, gBrowser);
-        addEventListener('dragstart', handleLongPress, true, gBrowser);
+    ['mousedown', 'mouseup', 'dragstart'].forEach(function(type) { addEventListener(type, handleLongPress, true, gBrowser) });
 
 })();
