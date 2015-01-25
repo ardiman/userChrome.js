8d33f30acddcc31a7c5aa4aeeefe3f74b8bd66d3
ardiman <sobotnik@gmx.net>
2015-01-25 11:14:47 +0100

    Skript - Update fuer neuere Fx-Versionen und mehrere Drucker

--- removeprinttime/removePrintTime.uc.js
+++ removeprinttime/removePrintTime.uc.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name           removePrintTime.uc.js
-// @compatibility  Firefox 10.*, Firefox 11.*
+// @compatibility  Firefox 35.*
 // @include        main
 // @version        1.0.20120318
 // ==/UserScript==
 
 var removePrintTime = {
   // Beginn der Konfiguration (der Druckername muss bestimmt geändert werden)
-  printerName: 'printer_Brother_HL-2170W_series',
+  printerNames: ['printer_Drucker_A', 'printer_Drucker_B', 'printer_Drucker_C'],
   // Ende der Konfiguration
   searchInPrefs: ['print_footerright','print_footercenter','print_footerleft','print_headerright','print_headercenter','print_headerleft'],
 
@@ -19,15 +19,19 @@ var removePrintTime = {
     var datString= ((D.getDate()<10) ? "0" : "") + D.getDate() + "."+ month + "." + year;
     this.setDate(datString);
   },
+
   setDate: function(d) {
     var prefManager = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
-    var Datummuster = /^(31|30|[012]\d|\d)\.(0\d|1[012]|\d)\.(\d{1,4})$/;
-    for (var i = 0; i < this.searchInPrefs.length; i++) {
-     var iPref=this.printerName+'.'+this.searchInPrefs[i];
-     var vPref = prefManager.getCharPref(iPref);
-     if (vPref == "&D" || Datummuster.test(vPref)) {
-       prefManager.setCharPref(iPref, d);
-     }
+    var Datummuster = /^(31|30|[012]\d|\d)\.(0\d|1[012]|\d)\.(\d{1,4})$/;   
+    for (var j = 0; j < this.printerNames.length; j++) {
+      var printerName = this.printerNames[j];
+      for (var i = 0; i < this.searchInPrefs.length; i++) {
+        var iPref = 'print.' + printerName + '.' + this.searchInPrefs[i];
+        var vPref = prefManager.getCharPref(iPref);
+        if (vPref == "&D" || Datummuster.test(vPref)) {
+          prefManager.setCharPref(iPref, d);
+        }
+      }
     }
   }
 };
