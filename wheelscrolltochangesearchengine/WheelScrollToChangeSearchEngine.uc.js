11d94a854787559a27ac421f3aad14d346117d6e
ardiman <sobotnik@gmx.net>
2015-03-22 17:30:13 +0100

    Skript - technisches Update

--- wheelscrolltochangesearchengine/WheelScrollToChangeSearchEngine.uc.js
+++ wheelscrolltochangesearchengine/WheelScrollToChangeSearchEngine.uc.js
@@ -3,13 +3,14 @@
 // @description    検索バー上のホイールスクロールで検索エンジンを変える
 // @version        1.0
 // @author         oflow
-// @compatibility  Firefox 4.0, 11.0
+// @compatibility  Firefox 4.0, 11.0, ESR31.3
 // @namespace      http://oflow.me/
+// @note           Firefox ESR 31.3で確認, unload時のremoveEventListener修正
 // @note           Firefox 11.0で動作確認
 // ==/UserScript==
 
 (function() {
-    var searchbar = document.getElementById('searchbar');
+    var searchbar = document.getElementById('searchbar') || BrowserSearch.searchBar;
     if (!searchbar) return;
     var changeSearchEngine = function(e) {
         if (!searchbar) {
@@ -20,7 +21,7 @@
     }
     searchbar.addEventListener('DOMMouseScroll', changeSearchEngine, false);
     window.addEventListener('unload', function() {
-        gBrowser.mTabContainer.removeEventListener('DOMMouseScroll', changeSearchEngine, false);
+        searchbar.removeEventListener('DOMMouseScroll', changeSearchEngine, false);
         window.removeEventListener('unload', arguments.callee, false);
     }, false);
 
