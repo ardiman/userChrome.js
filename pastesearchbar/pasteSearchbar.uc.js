036449b36367ca0733336b3ecc79b767eb0c9e20
ardiman <sobotnik@gmx.net>
2016-01-29 16:41:52 +0100

    Skript - neue Version

--- pastesearchbar/pasteSearchbar.uc.js
+++ pastesearchbar/pasteSearchbar.uc.js
@@ -1,25 +1,26 @@
 // ==UserScript==
 // @name         pasteSearchbar.uc.js
-// @description  Text aus Zwischenablage, mit Doppelklick in Suchleiste einfügen
+// @description  Text aus Zwischenablage, mit Doppeltklick in Suchleiste einfügen
 // @include      main
-// @version      v0.2 Bei Einfügen Suche automatisch starten (Standard = deaktiviert, true = aktiviert, false = deaktiviert)
-
+// @version      v0.3 Beim Einfügen die interne Suchbefehl Methode ersetzen
+// @version      v0.2 Beim Einfügen Suche automatisch starten (Standard = deaktiviert, true = aktiviert, false = deaktiviert)
 // ==/UserScript==
-(function () {
+(function() {
 
-	const SEARCH_GO = false; //Suchen automatisch starten = "true"
+	const SEARCH_GO = false; //"true" = Suchen automatisch starten
 
 	var searchbar = BrowserSearch.searchBar;
 	if (!searchbar) return;
 
-	var goButton = document.getAnonymousElementByAttribute(searchbar,"anonid", "search-go-button");
-
-	searchbar.addEventListener('dblclick', function (event) {
+	searchbar.addEventListener('dblclick', (event) => {
 		var str = readFromClipboard();
 		if (str) {
 			goDoCommand('cmd_paste');
-			if (SEARCH_GO) goButton.click();
+			if (SEARCH_GO) {
+				searchbar.select();
+				searchbar.handleSearchCommand();
+			}
 		}
-	}, false);
+	});
 
 }());
