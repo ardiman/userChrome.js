0d637092af647cd488462aed00b8897843493737
ardiman <sobotnik@gmx.net>
2016-07-03 10:13:28 +0200

    Skript - technisches Update

--- webdevelopertoolbarbutton/WebDeveloper-Menu.uc.js
+++ webdevelopertoolbarbutton/WebDeveloper-Menu.uc.js
@@ -1,21 +1,26 @@
-    //      Webdevelopercontextmenu.uc.js
+//   WebDeveloper-Menu.uc.js
 
-    (function() {
+(function() {
+   if (location != 'chrome://browser/content/browser.xul')
+      return;
+   setTimeout(function() {
+      var dblMenu = document.getElementById('webDeveloperMenu').cloneNode(true);
+      document.getElementById('contentAreaContextMenu').insertBefore(dblMenu, document.getElementById('context-viewsource'));
+      dblMenu.id = 'context-' + dblMenu.id;
+      var elements = dblMenu.getElementsByTagName('*');
+      for (let elem of elements) {
+         let origId = elem.id;
+         if (origId) {
+            elem.id = 'context-' + origId;
+            if (elem.tagName == 'menuitem') {
+               elem.setAttribute('oncommand', 'document.getElementById("' + origId + '").click();');
+               let obs = document.createElement('observes');
+               obs.setAttribute('element', origId);
+               obs.setAttribute('attribute', 'checked');
+               elem.appendChild(obs);
+            };
+         };
+      };
+   }, 100);
+}());
 
-       if (location != 'chrome://browser/content/browser.xul') return;
-          setTimeout( function() {
-
-       var contextMenu = document.getElementById('contentAreaContextMenu');
-       var dblMenu = document.importNode(document.getElementById('webDeveloperMenu'), true);
-    //   contextMenu.appendChild(document.createElement('menuseparator'));
-    //   contextMenu.appendChild(dblMenu);
-       contextMenu.insertBefore(dblMenu, document.getElementById('context-viewsource'));
-       dblMenu.id = 'context-' + dblMenu.id;
-       var elements = dblMenu.getElementsByTagName('*');
-       for (let i=0; i<elements.length; i++) {
-          var elem = elements[i];
-          if (elem.id)
-             elem.id = 'context-' + elem.id;
-       };
-    }, 0);
-    }());
