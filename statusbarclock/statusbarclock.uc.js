0486911768eec7a86667577d6d011111221b26aa
ardiman <sobotnik@gmx.net>
2017-11-24 16:33:21 +0100

    Update wg. Firefox 57 etc.

--- statusbarclock/statusbarclock.uc.js
+++ statusbarclock/statusbarclock.uc.js
@@ -1,34 +1,49 @@
-function Clock() {
-    var D = new Date();
-    var days = ["So","Mo", "Di", "Mi", "Do", "Fr", "Sa"];
-    var day = days[D.getDay()];
-    var hours = D.getHours();
-    var dateString = day + ", " + ("0"+D.getDate()).substr(-2) + "." + ("0"+(D.getMonth()+1)).substr(-2) + "." + D.getFullYear();
-    var timeString = ("0"+hours).substr(-2) + ":" + ("0"+D.getMinutes()).substr(-2) + " Uhr";
-    var statusC = document.getElementById("statusbar-clock-display");
-    statusC.setAttribute("label", dateString + " - " + timeString);
-    if (hours < 6 || hours > 22) {
-      statusC.setAttribute("style", "color: #ff0000;");
-      }
-     else {
-      statusC.setAttribute("style", "color: #000000;");
-     }
-     // Timeout variabel - damit Umschalten zur vollen Minute:
-     setTimeout("Clock()", (60- D.getSeconds())*1000);
-  }
-if (location == "chrome://browser/content/browser.xul") {
-  // moegliche IDs von Statusbars (je nach Firefox-Version und verwendeter Erweiterung)
-  var arrStatusbars = ["ctraddon_addon-bar","status4evar-status-bar","status-bar","addon-bar"];
-  for (i = 0; i < arrStatusbars.length; i++) {
-    if (document.getElementById(arrStatusbars[i]) !== null) {
-      // letztes Kindelement der Statusbar finden
-      var ClockStatus = document.getElementById(arrStatusbars[i]).lastChild;
-      break;
-    }
-  }
-  var ClockPanel = document.createElement("statusbarpanel");
-  ClockPanel.setAttribute("id", "statusbar-clock-display");
-  // *vor* dem letzten Kindelement (s.o.) einfuegen:
-  ClockStatus.parentNode.insertBefore(ClockPanel,ClockStatus.previousSibling);
-  Clock();
+function doDatClockCallback() { try{ doDatClock(); } catch(ex){} }
+
+function doDatClock() {
+	var options = { 
+		weekday: 'long', 
+		year: 'numeric', 
+		month: 'long', 
+		day: 'numeric', 
+		hour: '2-digit', 
+		minute: '2-digit', 
+		second: '2-digit' 
+	};
+	
+	var timestr = new Date().toLocaleDateString('de-DE',options);
+
+	var ua = window.navigator.userAgent;
+	var FFstr = ua.split(' ');
+	var FF = FFstr[FFstr.length-1].replace( '/' , ' ' );
+	
+	var text = FF + ' ';
+
+	var agent = document.getElementById('statusbar-agent-display');
+	agent.setAttribute( 'value', text );
+	
+	var status = document.getElementById('statusbar-clock-display');
+	status.setAttribute( 'value', timestr );
+	
+	window.setTimeout(doDatClockCallback, 1000);	
 }
+
+var css = 'padding-top: 4px !important; padding-left: 5px; color: yellow; font-weight: 700; text-shadow: none;';
+
+
+var ClockStatus = document.getElementById('helpMenu');
+
+var AgentLabel = document.createElement('label');
+AgentLabel.setAttribute('id', 'statusbar-agent-display');
+AgentLabel.setAttribute('class', 'statusbarpanel-text');
+AgentLabel.setAttribute('style', css);
+
+var ClockLabel = document.createElement('label');
+ClockLabel.setAttribute('id', 'statusbar-clock-display');
+ClockLabel.setAttribute('class', 'statusbarpanel-text');
+ClockLabel.setAttribute('style', css);	
+
+ClockStatus.parentNode.insertBefore(ClockLabel, ClockStatus.nextSibling);
+ClockStatus.parentNode.insertBefore(AgentLabel, ClockStatus.nextSibling);
+
+doDatClock();
